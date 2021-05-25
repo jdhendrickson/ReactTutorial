2300e5ff59ee939596feb71a48af149459dcb889
Joey <Joey@DESKTOP-G03B29I>
2021-05-25 14:47:13 -0700

    Added a function to calculate the winner

--- src/index.js
+++ src/index.js
@@ -91,6 +91,32 @@ class Game extends React.Component {
     }
 }
 
+/**
+* Helper function to calculate winner
+*/
+function calculateWinner(squares) {
+//All possible winning combinations
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  //Go through all winning combinations
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    //If squares in winning combination locations are the same
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];//Winner has been found
+    }
+  }
+  return null;
+}
+
 // ========================================
 
 ReactDOM.render(
